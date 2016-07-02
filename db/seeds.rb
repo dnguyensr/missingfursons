648be1fcb00b0ddaa33c552a13057c64aa01a278
[01;31m[K"name"[m[K: "Martha Rodgers <[01;31m[K"email"[m[K: "mrodgers25@gmail.com>
2016-07-02 12:56:19 -0700

    seeded database with 2 inital values and 2 posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(name: 'a', email: 'a@a.com', password: 'password')
+User.delete_all
+Species.delete_all
+Breed.delete_all
+Animal.delete_all
+Post.delete_all
+
+
+john = User.create(name: 'john', email: 'a@a.com', password: 'password')
+jane = User.create(name: 'jane', email: 'b@b.com', password: 'password')
+
+dog = Species.create(name: 'Dog')
+cat = Species.create(name: 'Cat')
+
+golden = Breed.create(name: 'Golden Retriever', species_id: dog.id)
+shih_tzu = Breed.create(name: 'Shih Tzu', species_id: dog.id)
+
+persy = Breed.create(name: 'Persian', species_id: cat.id)
+mix = Breed.create(name: 'Mixed', species_id: cat.id)
+
+teddy = Animal.create(breed_id: golden.id, name: "Teddy", color: "Blonde", additional_notes: "loves frisbees and peanut butter", image: File.new("./db/Teddy-2.jpeg"), age: 4, size: "medium" )
+fluffy = Animal.create(breed_id: mix.id, name: "Fluffy", color: "Calico", additional_notes: "loves chasing dogs", image: File.new("./db/Feles.jpg"), age: 2, size: "small" )
+
+newpost1 = Post.create(user_id: john.id, animal_id: teddy.id, found_status: false, location: "92117", phone: "619-884-0246", email: john.email, date: Date.today )
+newpost2 = Post.create(user_id: jane.id, animal_id: fluffy.id, found_status: false, location: "92101", phone: "619-884-0246", email: jane.email, date: Date.today )
+
+
