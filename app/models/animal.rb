21badcf9c5aa370061673fd4e7d0ef3afb155e8f
Noah Naamad <email": null>
2016-07-01 20:04:26 -0700

    add paperclip functionality, having issues with image being an attribute of animal rather than post

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,6 @@
 class Animal < ActiveRecord::Base
   has_many :breeds
   validates :name, :color, :size, presence: true
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100#" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
