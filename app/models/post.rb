a4bfd7abd9674d766c0501dfee8a1626f4cf267e
[01;31m[K"name"[m[K: "Taylor Beck <[01;31m[K"email"[m[K: "taylorgbeck@gmail.com>
2016-07-03 13:56:55 -0700

    Facebook integration waiting on post creation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,22 @@
 class Post < ActiveRecord::Base
+  include FacebookHelper
+
   belongs_to :user
   has_one :animal
   validates :location, presence: true
   after_initialize :set_default_found_status
+  after_save :post_to_facebook
 
   def set_default_found_status
     self.found_status = false
   end
+
+  def post_to_facebook
+    facebook_graph.put_wall_post("Help! My dog is lost! \#MissingFursons", {
+      "name" => "Have you seen him?",
+      "link" => "http://missingfursons.com",
+      "picture" => "http://i.imgur.com/fjXJEz1.jpg"
+    })
+  end
+
 end
