2fea14eae44ee67ac535c1905c79f8e7227fb6e4
[01;31m[K"name"[m[K: "Taylor Beck <[01;31m[K"email"[m[K: "taylorgbeck@gmail.com>
2016-07-03 15:11:03 -0700

    Begin Twitter integration

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,18 @@
 class Post < ActiveRecord::Base
   include FacebookHelper
+  include TwitterHelper
 
   belongs_to :user
   has_one :animal
   validates :location, presence: true
   after_initialize :set_default_found_status
-  after_save :post_to_facebook
+  after_save :post_to_facebook, :post_to_twitter
 
   def set_default_found_status
     self.found_status = false
   end
+
+  private
 
   def post_to_facebook
     facebook_graph.put_wall_post("Help! My dog is lost! \#MissingFursons", {
@@ -17,6 +20,10 @@ class Post < ActiveRecord::Base
       "link" => "http://missingfursons.com",
       "picture" => "http://i.imgur.com/fjXJEz1.jpg"
     })
+  end
+
+  def post_to_twitter
+    twitter_client.update("Lost dog in ZIP, @missing_fursons")
   end
 
 end
