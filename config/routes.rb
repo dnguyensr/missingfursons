618eb47ed746dc6dee850fee63c94a737e6d5668
Noah Naamad <email": null>
2016-07-04 20:27:41 -0700

    found a pet resources page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
       }
 
   get 'home2' => 'home#index'
+  get 'resource_found' => 'posts#resourecefound'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
