233b4dd97422fd64a712df3a06e814746c1cad86
Daniel Nguyen <email": null>
2016-07-04 18:46:49 -0700

    Update routing for user flow

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
         sessions: 'users/sessions'
       }
 
-  get 'home2' => 'home#index'
+  get 'alert' => 'home#alert'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
