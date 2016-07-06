e872d2153ab27b2b8ba307e4db3effd6d2a2c4c0
[01;31m[K"name"[m[K: "Taylor Beck <[01;31m[K"email"[m[K: "taylorgbeck@gmail.com>
2016-07-06 09:51:19 -0700

    Continue testing json with ios

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,11 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    # @posts = Post.all
-    # render '/posts/homepage2'
+    @posts = Post.all
+  end
 
+  def index_json
+    # render json: {posts: Post.all, animals: Animal.all}
     render json: Post.all
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
   get 'resource_found' => 'posts#resource_found'
   get 'alert' => 'home#alert'
 
+  # JSON 
+  get 'posts_json' => 'posts#index_json'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
