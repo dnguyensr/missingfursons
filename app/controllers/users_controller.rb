63b48dd70a00c02b0e833e7d95479a8c9ae66567
Daniel Nguyen <email": null>
2016-07-06 18:27:32 -0700

    Update edit link in user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     if current_user != User.find(params[:id])
       redirect_to root_path
     end
+    @current_domain = "http://#{request.host}"
     @pets = (Animal.where("user_id = ?", current_user.id))
   end
 end
